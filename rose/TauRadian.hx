2aeb9864017179c76250a1c66322ef56b7b5d0ce
nanjizal <nanjizal@users.noreply.github.com>
2023-09-23 10:38:44 +0100

    Update TauRadian.hx

--- rose/TauRadian.hx
+++ rose/TauRadian.hx
@@ -7,7 +7,7 @@ function get_epsilon(): Float {
 var pie_2( get, never ): Float;
 inline
 function get_pie_2(): Float {
-	return pie/2;
+    return pie/2;
 }
 var tau3_4( get, never ): Float;
 inline
@@ -30,7 +30,7 @@ function tauDivisor( val: Float ): Float {
 }
 inline
 function minusTau( val: Float ): Float {
-  var divisor = tauDivisor( val );
+    var divisor = tauDivisor( val );
 	return ( divisor>0)? tau*divisor: 0;
 }
 inline
@@ -39,47 +39,47 @@ function tauNormalize( val: Float ): Float {
 }
 inline
 function clean0( val: Float ){
-  return ( 
-    ( val > tau - epsilon && val < tau + epsilon )
-    ||
-    ( val > - epsilon && val < epsilon ) 
-  )? 0.: val;
+    return ( 
+        ( val > tau - epsilon && val < tau + epsilon )
+        ||
+        ( val > - epsilon && val < epsilon ) 
+    )? 0.: val;
 }
 inline
 function tauLimit( val: Float, zero: Bool = false ): Float {
-  var out = ( val >= 0 )? tauNormalize( val ): tau-tauNormalize( -val );
-  return ( zero )? clean0(out): out;
+    var out = ( val >= 0 )? tauNormalize( val ): tau-tauNormalize( -val );
+    return ( zero )? clean0(out): out;
 }
 inline
 function pi2piLimit( val: Float ): Float {
-  return if( val >= 0 ){
-    var temp = tauNormalize( val );
-    ( temp > Math.PI )? -2*Math.PI + temp: temp;
-  } else {
-	var temp = -tauNormalize( -val );
-    ( temp < -Math.PI )? temp + 2*Math.PI: temp;
-  }
+    return if( val >= 0 ){
+        var temp = tauNormalize( val );
+        ( temp > Math.PI )? -2*Math.PI + temp: temp;
+    } else {
+	    var temp = -tauNormalize( -val );
+        ( temp < -Math.PI )? temp + 2*Math.PI: temp;
+    }
 }
 inline
 function smallest( v1: Float, v2: Float ){
-	var t1 = tauLimit( v1, true );
-  var t2 = tauLimit( v2, true );
-  var dif: Float;
-  return if( t1 == t2 ){
-	0.;
-  } else if( t1 < t2 ){
-	dif = t2 - t1;
-    if( dif < Math.PI ){
-			dif;
+    var t1 = tauLimit( v1, true );
+    var t2 = tauLimit( v2, true );
+    var dif: Float;
+    return if( t1 == t2 ){
+	    0.;
+    } else if( t1 < t2 ){
+	    dif = t2 - t1;
+        if( dif < Math.PI ){
+		dif;
+        } else {
+	        -(2*Math.PI-dif);
+        }
     } else {
-			-(2*Math.PI-dif);
+        dif = t1 - t2;
+        if( dif < Math.PI ){
+		    -dif;
+        } else {
+		    (2*Math.PI-dif);
+        }
     }
-  } else {
-    dif = t1 - t2;
-    if( dif < Math.PI ){
-		-dif;
-    } else {
-		(2*Math.PI-dif);
-    }
-  }
 }
